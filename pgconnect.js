ada982bcf7841fba726c8462b1396aea93cc1ce5
amStevee <stevenanongo2@gmail.com>
2023-02-21 01:20:42 +0100

    pgconnect

--- pgconnect.js
+++ pgconnect.js
@@ -3,10 +3,10 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "daysdreamblog",
-  password: "793254",
+  user: "days_dream_db_user",
+  host: "dpg-cfq07gcgqg41dhrsra4g-a",
+  database: "days_dream_db",
+  password: "kFDYpsVkVqUCixS41ECSwjTiJB4j1WqZ",
   port: 5432,
 });
 
