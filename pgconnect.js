375007131792fa0e8db5c7abd92c6042fc03c5f0
amStevee <stevenanongo2@gmail.com>
2023-02-22 01:00:50 +0100

    udated pgconnect to railway

--- pgconnect.js
+++ pgconnect.js
@@ -3,11 +3,11 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const pool = new Pool({
-  user: "days_dream_db_user",
-  host: "dpg-cfq07gcgqg41dhrsra4g-a.oregon-postgres.render.com",
-  database: "days_dream_db",
-  password: "kFDYpsVkVqUCixS41ECSwjTiJB4j1WqZ",
-  port: 5432,
+  user: process.env.PGUSER || "postgres",
+  host: process.env.PGHOST || "containers-us-west-144.railway.app",
+  database: process.env.PGDATABASE || "railway",
+  password: process.env.PGPASSWORD || "UdO2wsYNn8Xt8zOfOIMp",
+  port: process.env.PGPORT || 7124,
 });
 
 module.exports = pool;
