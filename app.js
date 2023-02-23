dfd61ca19ebcecf8b8c5460bd0bbe9ab8a1ead83
amStevee <stevenanongo2@gmail.com>
2023-02-24 00:37:40 +0100

    cors

--- app.js
+++ app.js
@@ -14,7 +14,15 @@ dotenv.config();
 let dir = path.join(__dirname, "uploads");
 
 app.use(morgan("dev"));
-app.use(cors({ origin: "https://daydreamblog.netlify.app/" }));
+app.use(cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requsted-With, Content-Type, Accept"
+  );
+  next();
+});
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
