c98e817a26499e0e6b251fe297e277b87879c6cd
amStevee <stevenanongo2@gmail.com>
2023-02-24 01:18:21 +0100

    fixing cookies

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ dotenv.config();
 let dir = path.join(__dirname, "uploads");
 
 app.use(morgan("dev"));
-app.use(cors());
+app.use(cors({ credentials: true }));
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
@@ -24,8 +24,8 @@ app.use((req, res, next) => {
   next();
 });
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(dir));
 
 const storage = multer.diskStorage({
