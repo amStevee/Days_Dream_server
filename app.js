ad4c14394b2c007550aaa7d20a09d50d43d9279a
amStevee <stevenanongo2@gmail.com>
2023-02-24 23:37:36 +0100

    cokkie/cors

--- app.js
+++ app.js
@@ -15,7 +15,6 @@ let dir = path.join(__dirname, "uploads");
 
 app.use(morgan("dev"));
 app.use(cookieParser());
-app.use(cors());
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
@@ -25,6 +24,7 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Credentials", true);
   next();
 });
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(dir));
