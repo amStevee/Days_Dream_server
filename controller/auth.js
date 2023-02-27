c94932b0a9ddf4c03e2b91f1fb8360ce6e2249f0
amStevee <stevenanongo2@gmail.com>
2023-02-27 11:47:07 +0100

    accessT

--- controller/auth.js
+++ controller/auth.js
@@ -67,7 +67,7 @@ const login = async (req, res, next) => {
     const { password, ...others } = isUser.rows[0];
 
     if (cook === null || undefined) {
-      res
+      return res
         .cookie("access_token", token, { httpOnly: true })
         .status(200)
         .json({ ...others });
