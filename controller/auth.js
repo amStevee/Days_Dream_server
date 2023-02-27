f306f7d7e68e94b096b1902f23dc36283a166bc3
amStevee <stevenanongo2@gmail.com>
2023-02-27 17:10:22 +0100

    test header

--- controller/auth.js
+++ controller/auth.js
@@ -79,7 +79,7 @@ const login = async (req, res, next) => {
 
 const logout = (req, res) => {
   res
-    .clearCookie("jwt", {
+    .clearCookie("access_token", {
       sameSite: "none",
       secure: true,
     })
