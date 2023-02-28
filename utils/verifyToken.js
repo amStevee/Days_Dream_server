c15bc102d75909ceeb06da7c5497e66d130919d0
amStevee <stevenanongo2@gmail.com>
2023-02-28 22:41:18 +0100

    log req.headers

--- utils/verifyToken.js
+++ utils/verifyToken.js
@@ -2,6 +2,7 @@ const jwt = require("jsonwebtoken");
 const createError = require("./error");
 
 const verifyToken = (req, res, next) => {
+  console.log(req.headers);
   const token = req.cookies["access_token"];
   if (!token) return next(createError(401, "you are not authenticated"));
   jwt.verify(token, process.env.JWTHASH, (err, user) => {
