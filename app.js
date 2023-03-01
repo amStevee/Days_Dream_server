2f964ee4b6e51900c59a68280caa6b803311d0b0
amStevee <stevenanongo2@gmail.com>
2023-03-01 17:44:32 +0100

    made changes to cors middle ware

--- app.js
+++ app.js
@@ -17,16 +17,17 @@ let dir = path.join(__dirname, "uploads");
 app.use(morgan("dev"));
 app.use(cors());
 app.use((req, res, next) => {
-  const url = req.headers.origin || "https://localhost:3000";
+  const url = req.headers.origin || "http://localhost:3000";
+
   res.setHeader("Access-Control-Allow-Origin", url);
   res.setHeader(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type,Accept,Authorization"
+    "Origin, X-Requested-With, Content-Type, Accept,Authorization"
   );
-  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Allow-Credentials", true);
   res.setHeader(
     "Access-Control-Allow-Methods",
-    "GET,POST,PUT,PATCH,DELETE,OPTIONS"
+    "GET,POST,PATCH,DELETE,OPTIONS"
   );
   next();
 });
