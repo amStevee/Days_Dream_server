d5fa09304dc13e580d27a7ec19e5010dfa0a7d43
amStevee <stevenanongo2@gmail.com>
2023-03-01 17:40:43 +0100

    made changes to cors middle ware

--- routes/posts.js
+++ routes/posts.js
@@ -11,7 +11,7 @@ const router = express.Router();
 
 router.route("/").get(getPosts).post(addPosts);
 router.route("/:id").get(getPost);
-router.delete("/:id", deletePosts);
-router.put("/:id", updatePosts);
+router.delete("/:id", verifyToken, deletePosts);
+router.put("/:id", verifyToken, updatePosts);
 
 module.exports = router;
