1becaaadcaf3b3dd904fd781d41fdee7e9446802
amStevee <stevenanongo2@gmail.com>
2023-03-04 21:34:42 +0100

    min

--- controller/posts.js
+++ controller/posts.js
@@ -63,7 +63,7 @@ const deletePosts = async (req, res) => {
 
 const updatePosts = async (req, res) => {
   const { title, description, category, image, userid } = req.body;
-  const postId = req.params;
+  const postId = req.query.edit;
   console.log(userid);
   console.log(postId);
 
