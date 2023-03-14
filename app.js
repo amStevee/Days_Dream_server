52ccf7973c3cabb293e9642d56638dad8f3d2790
amStevee <stevenanongo2@gmail.com>
2023-03-14 20:15:27 +0100

    cookies

--- app.js
+++ app.js
@@ -15,14 +15,7 @@ const port = process.env.PORT || 3001;
 dotenv.config();
 
 app.use(morgan("dev"));
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", req.headers.origin);
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+
 const whitelist = [
   "https://daydreamblog.netlify.app",
   "https://s3.console.aws.amazon.com",
