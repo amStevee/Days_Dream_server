736abc8584b16995101bddcd90557bee1d90b8e1
amStevee <stevenanongo2@gmail.com>
2023-04-14 22:52:22 +0100

    updated s3 connection

--- pgconnect.js
+++ pgconnect.js
@@ -4,10 +4,10 @@ dotenv.config();
 
 const pool = new Pool({
   user: process.env.PGUSER || "postgres",
-  host: process.env.PGHOST || "containers-us-west-144.railway.app",
-  database: process.env.PGDATABASE || "railway",
-  password: process.env.PGPASSWORD || "UdO2wsYNn8Xt8zOfOIMp",
-  port: process.env.PGPORT || 7124,
+  host: process.env.PGHOST || "localhost",
+  database: process.env.PGDATABASE || "daydreamblog",
+  password: process.env.PGPASSWORD || "93254",
+  port: process.env.PGPORT || 5432,
 });
 
 module.exports = pool;
