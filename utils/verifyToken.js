fceb6f41c3672852dac7f2199a2ccd22b37164b8
amStevee <stevenanongo2@gmail.com>
2023-04-14 21:55:17 +0100

    updated s3

--- utils/verifyToken.js
+++ utils/verifyToken.js
@@ -3,7 +3,6 @@ const createError = require("./error");
 
 const verifyToken = (req, res, next) => {
   const token = req.cookies.access_token;
-  console.log(token);
   if (!token) return next(createError(401, "you are not authenticated"));
   jwt.verify(token, process.env.JWTHASH, (err, user) => {
     if (err) return next(createError(403, "Token not valid"));
