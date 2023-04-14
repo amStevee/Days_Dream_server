d91cdcabb4241d79ec91a2218aa8a3fc77405bab
amStevee <stevenanongo2@gmail.com>
2023-04-14 23:14:55 +0100

    fixing bugs in aws s3 connection

--- pgconnect.js
+++ pgconnect.js
@@ -4,9 +4,9 @@ dotenv.config();
 
 const pool = new Pool({
   user: process.env.PGUSER || "postgres",
-  host: process.env.PGHOST || "localhost",
+  host: process.env.PGHOST || "127.0.0.1",
   database: process.env.PGDATABASE || "daydreamblog",
-  password: process.env.PGPASSWORD || "",
+  password: process.env.PGPASSWORD || "793254",
   port: process.env.PGPORT || 5432,
 });
 
