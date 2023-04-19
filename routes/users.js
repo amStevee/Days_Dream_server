40792a3b880faabb4003ff34126c9e0477ee8f92
amStevee <stevenanongo2@gmail.com>
2023-04-19 02:06:48 +0100

    add removeUserAdmin

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,13 @@
 const express = require("express");
-const { makeUserAdmin, getUsers } = require("../controller/users");
+const {
+  makeUserAdmin,
+  getUsers,
+  removeUserAdmin,
+} = require("../controller/users");
 const router = express.Router();
 
 router.route("/").get(getUsers);
 router.route("/").post(makeUserAdmin);
-router.route("/").put(makeUserAdmin);
+router.route("/").put(removeUserAdmin);
 
 module.exports = router;
