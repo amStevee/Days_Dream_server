d710b11b3686b62680bf81602a3482904e7cbd89
amStevee <stevenanongo2@gmail.com>
2023-04-20 23:42:23 +0100

    add verify token

--- controller/auth.js
+++ controller/auth.js
@@ -1,16 +1,16 @@
-const pool = require("../pgconnect");
-const createError = require("../utils/error");
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
-const { v4: uuidv4 } = require("uuid");
+const pool = require('../pgconnect');
+const createError = require('../utils/error');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const { v4: uuidv4 } = require('uuid');
 
 const register = (req, res, next) => {
   // CHECK EXISTING user
-  const q = "SELECT * FROM users WHERE email = $1 OR username = $2";
+  const q = 'SELECT * FROM users WHERE email = $1 OR username = $2';
   pool.query(q, [req.body.input.email, req.body.input.name], (err, data) => {
     if (err) return next(createError(404));
     if (data.rows.length) {
-      return res.status(409).json({ msg: "User alraeady exists" });
+      return res.status(409).json({ msg: 'User alraeady exists' });
     } else {
       // HASH PASSWORD
       const salt = bcrypt.genSaltSync(10);
@@ -19,7 +19,7 @@ const register = (req, res, next) => {
       // CREATE USER
       const userId = uuidv4();
       const qq =
-        "INSERT INTO users(userid, username, email, password, user_image) VALUES($1, $2, $3, $4, $5)";
+        'INSERT INTO users(userid, username, email, password, user_image) VALUES($1, $2, $3, $4, $5)';
       pool.query(
         qq,
         [
@@ -33,7 +33,7 @@ const register = (req, res, next) => {
           if (err) return res.json(err.message);
           return res
             .status(200)
-            .json({ msg: "you have been registered successfully" });
+            .json({ msg: 'you have been registered successfully' });
         }
       );
     }
@@ -41,16 +41,16 @@ const register = (req, res, next) => {
 };
 
 const login = async (req, res, next) => {
-  const q = "SELECT * FROM users WHERE username = $1";
+  const q = 'SELECT * FROM users WHERE username = $1';
 
   try {
     const isUser = await pool.query(q, [req.body.username]);
 
-    if (!isUser.rows[0]) return res.status(404).json({ msg: "usernot found" });
+    if (!isUser.rows[0]) return res.status(404).json({ msg: 'usernot found' });
     if (req.body.username === {} || req.body.password === {}) {
       return res
         .status(400)
-        .json({ msg: "please fill out the fileds in other to login" });
+        .json({ msg: 'please fill out the fileds in other to login' });
     }
 
     // COMPARE PASSWORD
@@ -59,10 +59,10 @@ const login = async (req, res, next) => {
       isUser.rows[0].password
     );
     if (!cPassword) {
-      return res.status(400).json({ msg: "incorrect username or password" });
+      return res.status(400).json({ msg: 'incorrect username or password' });
     }
     const token = jwt.sign({ id: isUser.rows[0].userid }, process.env.JWTHASH, {
-      expiresIn: "24h",
+      expiresIn: '24h',
     });
     const { password, ...others } = isUser.rows[0];
 
@@ -77,11 +77,9 @@ const login = async (req, res, next) => {
       options.domain = req.hostname;
     }
 
-    res
-      .cookie("access_token", token, options)
+    res.cookie('access_token', token, options);
 
-      res
-      .json({ ...others });
+    res.json({ ...others });
   } catch (error) {
     return res.status(500).json({ msg: error.message });
   }
@@ -89,46 +87,46 @@ const login = async (req, res, next) => {
 
 const logout = (req, res) => {
   res
-    .clearCookie("access_token", {
-      sameSite: "none",
+    .clearCookie('access_token', {
+      sameSite: 'none',
       secure: true,
     })
     .status(200)
-    .json({ msg: "user has been loged out" });
+    .json({ msg: 'user has been loged out' });
 };
 
 const updateUser = (req, res) => {
   const { username, email, image, userid } = req.body;
 
   if (username) {
-    const q = "UPDATE users SET username = $1 WHERE userid = $2";
+    const q = 'UPDATE users SET username = $1 WHERE userid = $2';
     pool.query(q, [username, userid], (err, qdata) => {
       if (err) {
         return res.status(400).json({ msg: err.message });
       }
-      res.status(200).json({ msg: "username has been updated successfully" });
+      res.status(200).json({ msg: 'username has been updated successfully' });
     });
   }
 
   if (email) {
-    const q = "UPDATE users SET email = $1 WHERE userid = $2";
+    const q = 'UPDATE users SET email = $1 WHERE userid = $2';
     pool.query(q, [email, userid], (err, qdata) => {
       if (err) {
         return res.status(400).json({ msg: err.message });
       }
-      res.status(200).json({ msg: "user email has been updated successfully" });
+      res.status(200).json({ msg: 'user email has been updated successfully' });
     });
   }
 
   if (image) {
-    const q = "UPDATE users SET user_image = $1 WHERE userid = $2";
+    const q = 'UPDATE users SET user_image = $1 WHERE userid = $2';
     pool.query(q, [image, userid], (err, qdata) => {
       if (err) {
         return res.status(400).json({ msg: err.message });
       }
       res
         .status(200)
-        .json({ msg: "user picture has been updated successfully" });
+        .json({ msg: 'user picture has been updated successfully' });
     });
   }
 };
