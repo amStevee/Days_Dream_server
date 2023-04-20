373bba789f59f12e7b766b14927f0c5f7054b2bd
amStevee <stevenanongo2@gmail.com>
2023-04-20 22:17:47 +0100

    add token

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const { register, login, logout, updateUser } = require("../controller/auth");
+const { verify } = require("jsonwebtoken");
 const router = express.Router();
 
 router.post("/register", register);
-router.post("/login", login);
+router.post('/login', login);
 router.post("/logout", logout);
-router.put("/update", updateUser);
+router.put('/update', verify, updateUser);
 
 module.exports = router;
